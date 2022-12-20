1754bd1c340b43c9c4ef98e1f6dd78a1e81c4bd5
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-12-20 19:55:50 +0300

    change 01-dz02.py

--- Seminar_01/Homework/01-dz02.py
+++ Seminar_01/Homework/01-dz02.py
@@ -6,5 +6,5 @@ for w in range(2):
     for x in range(2):
         for y in range(2):
             for z in range(2):
-                if (w and z) or (not y) or ((not x) == (not w)):
+                if not (w and z) or (not y) or ((not x) == (not w)):
                     print(w, x, y, z)
